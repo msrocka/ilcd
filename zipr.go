d024a3c18cb2bd9214b2034288f2453789ec7bf4
msrocka <michael.srocka@gmail.com>
2017-11-23 10:03:39 +0100

    zip reader: support more data types

--- zipr.go
+++ zipr.go
@@ -134,8 +134,7 @@ func (r *ZipReader) EachProcess(handler func(*Process) bool) error {
 	for _, f := range r.r.File {
 		if IsProcessPath(f.Name) {
 			process := &Process{}
-			err := unmarshal(f, process)
-			if err != nil {
+			if err := unmarshal(f, process); err != nil {
 				return err
 			}
 			if !handler(process) {
@@ -152,11 +151,95 @@ func (r *ZipReader) EachFlow(fn func(*Flow) bool) error {
 	for _, f := range r.r.File {
 		if IsFlowPath(f.Name) {
 			flow := &Flow{}
-			err := unmarshal(f, flow)
-			if err != nil {
+			if err := unmarshal(f, flow); err != nil {
 				return err
 			}
 			if !fn(flow) {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// EachMethod iterates over each LCIA method data set in the package unless
+// the given handler returns false.
+func (r *ZipReader) EachMethod(fn func(*Method) bool) error {
+	for _, f := range r.r.File {
+		if IsMethodPath(f.Name) {
+			m := &Method{}
+			if err := unmarshal(f, m); err != nil {
+				return err
+			}
+			if !fn(m) {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// EachFlowProperty iterates over each flow property data set in the package unless
+// the given handler returns false.
+func (r *ZipReader) EachFlowProperty(fn func(*FlowProperty) bool) error {
+	for _, f := range r.r.File {
+		if IsFlowPropertyPath(f.Name) {
+			fp := &FlowProperty{}
+			if err := unmarshal(f, fp); err != nil {
+				return err
+			}
+			if !fn(fp) {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// EachUnitGroup iterates over each unit group data set in the package unless
+// the given handler returns false.
+func (r *ZipReader) EachUnitGroup(fn func(*UnitGroup) bool) error {
+	for _, f := range r.r.File {
+		if IsUnitGroupPath(f.Name) {
+			ug := &UnitGroup{}
+			if err := unmarshal(f, ug); err != nil {
+				return err
+			}
+			if !fn(ug) {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// EachContact iterates over each contact data set in the package unless
+// the given handler returns false.
+func (r *ZipReader) EachContact(fn func(*Contact) bool) error {
+	for _, f := range r.r.File {
+		if IsContactPath(f.Name) {
+			c := &Contact{}
+			if err := unmarshal(f, c); err != nil {
+				return err
+			}
+			if !fn(c) {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// EachSource iterates over each source data set in the package unless
+// the given handler returns false.
+func (r *ZipReader) EachSource(fn func(*Source) bool) error {
+	for _, f := range r.r.File {
+		if IsSourcePath(f.Name) {
+			s := &Source{}
+			if err := unmarshal(f, s); err != nil {
+				return err
+			}
+			if !fn(s) {
 				break
 			}
 		}
